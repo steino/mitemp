a822dadd268ad81b24a8a51718ad570e6ad7383d
Stein Ivar Berghei <stein-ivar@berghei.no>
2019-02-22 20:16:43 +0100

    Add support for multiple devices and a config file

--- mitemp.py
+++ mitemp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from mitemp_bt.mitemp_bt_poller import MiTempBtPoller
 from btlewrap.bluepy import BluepyBackend
@@ -7,61 +8,75 @@ import paho.mqtt.client as mqtt
 import time
 import json
 import logging
+import yaml
 
 _LOGGER = logging.getLogger(__name__)
 
-
-poller = MiTempBtPoller('4c:65:a8:db:91:ef', cache_timeout=300, backend=BluepyBackend)
-
-poller.ble_timeout = 10
-poller.retries = 2
+with open("config.yaml", 'r') as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        sys.exit(1)
 
 client = mqtt.Client()
-client.connect("192.168.1.138", 1883, 60)
+client.connect(config["mqtt"]["host"], config["mqtt"]["port"], 60)
 
-tr = json.dumps({
-    'device_class': 'temperature',
-    'name': 'Xiaomi Stue Temperature',
-    'state_topic': 'homeassistant/sensor/xiaomi/stue/state',
-    'unit_of_measurement': '°C',
-    'value_template': '{{ value_json.temperature }}'
-    })
+for device in config["devices"]:
+    device_id = list(device.keys())[0]
+    address = device[device_id]["address"]
+    name = device[device_id]["name"]
 
-hr = json.dumps({
-    'device_class': 'humidity',
-    'name': 'Xiaomi Stue Humidity',
-    'state_topic': 'homeassistant/sensor/xiaomi/stue/state',
-    'unit_of_measurement': '%',
-    'value_template': '{{ value_json.humidity }}'
-    })
+    if config["mqtt"]["discovery"]:
+        tr = json.dumps({
+            'device_class': 'temperature',
+            'name': '%s Temperature' % name,
+            'state_topic': '%s/sensor/xiaomi/%s/state' % (config["mqtt"]["state_topic"], device_id),
+            'unit_of_measurement': '°C',
+            'value_template': '{{ value_json.temperature }}'
+            })
 
-br = json.dumps({
-    'device_class': 'battery',
-    'name': 'Xiaomi Stue Battery',
-    'state_topic': 'homeassistant/sensor/xiaomi/stue/state',
-    'unit_of_measurement': '%',
-    'value_template': '{{ value_json.battery }}'
-    })
+        hr = json.dumps({
+            'device_class': 'humidity',
+            'name': '%s Humidity' % name,
+            'state_topic': '%s/sensor/xiaomi/%s/state' % (config["mqtt"]["state_topic"], device_id),
+            'unit_of_measurement': '%',
+            'value_template': '{{ value_json.humidity }}'
+            })
 
-client.publish("homeassistant/sensor/xiaomi/stue_temperature/config", tr, retain=True)
-client.publish("homeassistant/sensor/xiaomi/stue_humidity/config", hr, retain=True)
-client.publish("homeassistant/sensor/xiaomi/stue_battery/config", br, retain=True)
+        br = json.dumps({
+            'device_class': 'battery',
+            'name': '%s Battery' % name,
+            'state_topic': '%s/sensor/xiaomi/%s/state' % (config["mqtt"]["state_topic"], device_id),
+            'unit_of_measurement': '%',
+            'value_template': '{{ value_json.battery }}'
+            })
+
+        client.publish("%s/sensor/xiaomi/%s_temperature/config" % (config["mqtt"]["state_topic"], device_id), tr, retain=True)
+        client.publish("%s/sensor/xiaomi/%s_humidity/config" % (config["mqtt"]["state_topic"], device_id), hr, retain=True)
+        client.publish("%s/sensor/xiaomi/%s_battery/config" % (config["mqtt"]["state_topic"], device_id), br, retain=True)
+
+        device[device_id]["poller"] = MiTempBtPoller(address, cache_timeout=300, backend=BluepyBackend)
+
 
 client.loop_start()
 
 while True:
-    try:
-        r = json.dumps({
-            'temperature': poller.parameter_value("temperature"),
-            'humidity': poller.parameter_value("humidity"),
-            'battery': poller.parameter_value("battery")
-            })
-    except IOError as ioerr:
-        _LOGGER.warning("Polling error %s", ioerr)
-    except BluetoothBackendException as bterror:
-        _LOGGER.warning("Polling error %s", bterror)
-    finally:
-        client.publish("homeassistant/sensor/xiaomi/stue/state", r)
-                print(r)
+    for device in config["devices"]:
+        device_id = list(device.keys())[0]
+        poller = device[device_id]["poller"]
+        try:
+            r = json.dumps({
+                'temperature': poller.parameter_value("temperature"),
+                'humidity': poller.parameter_value("humidity"),
+                'battery': poller.parameter_value("battery")
+                })
+        except IOError as ioerr:
+            _LOGGER.warning("Polling error %s", ioerr)
+        except BluetoothBackendException as bterror:
+            _LOGGER.warning("Polling error %s", bterror)
+        finally:
+            client.publish("%s/sensor/xiaomi/%s/state" % (config["mqtt"]["state_topic"], device_id), r)
+            print(r)
 
-        time.sleep(300)
+    time.sleep(300)
